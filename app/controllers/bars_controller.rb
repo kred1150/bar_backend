95fa4270d94f29e9a6924c5e7bb06e21b37e8bab
Kevin Redmond <kred1150@gmail.com>
2023-07-24 17:52:29 -0500

    Added a route and show method to display json for a given bar based on user entered params.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -3,4 +3,9 @@ class BarsController < ApplicationController
     @bars = Bar.all
     render json: @bars.as_json
   end
+
+  def show
+    @bar = Bar.find_by(id: params["id"])
+    render json: @bar.as_json
+  end
 end
