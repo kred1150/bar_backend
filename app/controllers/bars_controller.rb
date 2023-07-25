3a436bd4e87e975cee92dd8e6f8c3eeaf25c51f2
Kevin Redmond <kred1150@gmail.com>
2023-07-25 14:16:16 -0500

    Added restFul routes for create, update and destroy for the Bars controller, added the corresponding routes to the route file, and created a requests.http page to test the routing params.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -8,4 +8,40 @@ class BarsController < ApplicationController
     @bar = Bar.find_by(id: params["id"])
     render json: @bar.as_json
   end
+
+  def create
+    @bar = Bar.new(
+      name: params["name"],
+      price: params["price"],
+      description: params["description"],
+    )
+    if @bar.save
+      Image.create(
+        url: params["image_url"],
+        bar_id: @bar.id,
+      )
+      render json: @bar.as_json
+    else
+      render json: { errors: @bar.errors.full_messages }, status: 422
+    end
+  end
+
+  def update
+    @bar = Bar.find_by(id: params["id"])
+    @bar.name = params["name"] || @bar.name
+    @bar.price = params["price"] || @bar.price
+    @bar.description = params["description"] || @bar.description
+
+    if @bar.save
+      render json: @bar.as_json
+    else
+      render json: { errors: @bar.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    @bar = Bar.find_by(id: params["id"])
+    @bar.destroy
+    render json: { message: "You have successfuly deleted this bar!" }
+  end
 end
