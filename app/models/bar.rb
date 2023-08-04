e6b2ed1a71bba358950f82b68667a83ba101de10
Kevin Redmond <kred1150@gmail.com>
2023-08-04 12:53:22 -0500

    Added has_many belongs_to relationships for Users and Bars.

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -5,6 +5,7 @@ class Bar < ApplicationRecord
   validates :description, length: { in: 10..500 }
 
   has_many :images
+  belongs_to :user
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
