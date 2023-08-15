f4e37d6b150125e6a7542de3bbc826ad09961ea4
Kevin Redmond <kred1150@gmail.com>
2023-08-15 13:29:38 -0500

    Revised authentication to use before_action in orders controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    if current_user
-      @orders = current_user.orders
-      render template: "orders/index"
-    else
-      render json: [], status: :unauthorized
-    end
+    @orders = current_user.orders
+    render template: "orders/index"
   end
 
   def create
-    if current_user
-      @order = Order.new(
-        user_id: current_user.id,
-        delivery: params[:delivery],
-        bar_id: params[:bar_id],
-        subtotal: params[:subtotal],
-        total: params[:total],
-      )
-      if @order.save
-        render template: "orders/show"
-      else
-        render json: { errors: @order.errors.full_messages }
-      end
+    @order = Order.new(
+      user_id: current_user.id,
+      delivery: params[:delivery],
+      bar_id: params[:bar_id],
+      subtotal: params[:subtotal],
+      total: params[:total],
+    )
+    if @order.save
+      render template: "orders/show"
     else
-      render json: [], status: :unauthorized
+      render json: { errors: @order.errors.full_messages }
     end
   end
 
   def show
-    if current_user
-      @order = current_user.orders.find_by(id: params["id"])
-      render template: "orders/show"
-    else
-      render json: [], status: :unauthorized
-    end
+    @order = current_user.orders.find_by(id: params["id"])
+    render template: "orders/show"
   end
 end
