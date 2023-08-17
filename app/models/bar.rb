5f02a3e4761d0728e55ac4c7b4d0cf80182a0b73
Kevin Redmond <kred1150@gmail.com>
2023-08-17 13:12:41 -0500

    Added has many through relationships for bars and categories through bar_categories.

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -4,10 +4,12 @@ class Bar < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
 
+  belongs_to :user
+
   has_many :images
   has_many :orders
-  belongs_to :user
   has_many :bar_categories
+  has_many :categories, through: :bar_categories
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
